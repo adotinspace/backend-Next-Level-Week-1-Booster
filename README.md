0cdfd3aa52bd1284aa540454fdc4c9bc9c90322f
adotinspace <shinryaku.desu@gmail.com>
2020-06-03 22:51:30 -0300

    second commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nextLevelWeek-1-backend
+# backend do projeto Next Level Week #1 edição Booster
---
+++ src/routes.ts
@@ -0,0 +1,26 @@
+import express, { request, response } from 'express';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+/**
+ * padrões de rota:
+ * index - listagem,
+ * show - exibir único registro,
+ * create,
+ * update,
+ * delete,
+ */
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points/', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+
+export default routes;
